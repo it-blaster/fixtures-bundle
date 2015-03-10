c72fce0728492242cc879f589d3a2c7603ad4012
0x2f8f <0x2f8f@gmail.com>
2015-03-10 23:32:57 +0500

    init symfony bundle

---
+++ ItBlasterFixturesBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ItBlaster\FixturesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ItBlasterFixturesBundle extends Bundle
+{
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# fixtures-bundle
+FixturesBundle
+====================
+
 Symfony2 bundle for propel fixtures
